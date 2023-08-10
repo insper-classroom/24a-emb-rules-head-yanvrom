a564973c6387a8a65bcde0d683552d86b479f7c3
Rafael Corsi <corsiferrao@gmail.com>
2023-08-10 17:40:55 -0300

    v0

---
+++ main.c
@@ -0,0 +1,7 @@
+// main.c
+#include "main.h"
+
+int main(void) {
+  int a = foo(12);
+  return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+// main.h
+int var;
+
+int foo(int b) {
+  if (b > 2)
+    return 0;
+  return 1;
+}
